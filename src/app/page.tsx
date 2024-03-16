2cf7e202c56fe8351fe8ffcbb9c06b88ad4f20e3
itskyuq <88845004+MyKiWx@users.noreply.github.com>
2024-03-16 21:06:10 +0700

    update project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,272 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @next/next/no-img-element */
+"use client";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import { Button } from "@chakra-ui/button";
+import { Icon } from "@chakra-ui/icon";
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  ListItem,
+  OrderedList,
+  Text,
+  VStack,
+} from "@chakra-ui/layout";
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+import { useEffect, useRef, useState } from "react";
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+import axios from "axios";
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+import { PiUploadBold } from "react-icons/pi";
+type Props = {};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+export interface DataType {
+  time: number;
+  image: Image;
+  predictions: Prediction[];
 }
+
+export interface Image {
+  width: number;
+  height: number;
+}
+
+export interface Prediction {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  confidence: number;
+  class: string;
+  class_id: number;
+  detection_id: string;
+}
+const Home = (props: Props) => {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const inputFile = useRef<HTMLInputElement>(null);
+  const [data, setData] = useState<DataType | undefined>(undefined);
+  const [price, setPrice] = useState(0);
+
+  const loadImageBase64 = (file: Blob | null) => {
+    if (file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+      });
+    }
+  };
+
+  const fileSelectHandler = (file: File | null) => {
+    if (file) {
+      // handle the file here
+      console.log("Selected file:", file);
+      setSelectedImage(file);
+    }
+  };
+
+  const bottleDetection = async () => {
+    const image = await loadImageBase64(selectedImage);
+
+    axios({
+      method: "POST",
+      url: "https://detect.roboflow.com/plastic-bottle-identifier/6",
+      params: {
+        api_key: process.env.NEXT_PUBLIC_API_KEY,
+      },
+      data: image,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+      .then(function (response) {
+        setData(response.data);
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
+  };
+
+  const onButtonClick = () => {
+    if (inputFile.current) {
+      inputFile.current.click();
+    }
+  };
+
+  useEffect(() => {
+    if (selectedImage) {
+      bottleDetection();
+    }
+  }, [selectedImage]);
+
+  useEffect(() => {
+    if (data) {
+      setPrice(data.predictions.length * 7);
+    }
+  }, [data]);
+  return (
+    <div>
+      <Box
+        px={8}
+        py={4}
+        borderRight="2px solid black"
+        borderBottom="2px solid black"
+        sx={{
+          h: "calc(100dvh - 64px)",
+          w: "calc(100dvw - 200px)",
+        }}
+      >
+        <Box bg="white" border="2px solid black" p={4}>
+          <Box w={20} h={4} mt={0} bg="#ffec99" border="2px solid black" />
+          <Box w="full" h={8} mt={2} bg="#ffd8a8" border="2px solid black" />
+          <Box w="full" h={16} mt={4} bg="#99e9f2" border="2px solid black" />
+          <Grid w="full" mt={4} templateColumns="repeat(5, 1fr)" gap={6}>
+            <GridItem w="full" colSpan={1}>
+              <Box
+                w="full"
+                h={32}
+                mt={0}
+                bg="#96f2d7"
+                border="2px solid black"
+              />
+              <Box
+                w="full"
+                // h={32}
+                py={2}
+                mt={2}
+                px={2}
+                bg="white"
+                border="2px solid black"
+              >
+                <Text fontSize="xs" fontWeight="bold" textAlign="center">
+                  Upload Image or a Video File
+                </Text>
+                <Box
+                  w="full"
+                  // h={24}
+                  py={4}
+                  mt={2}
+                  border="2px dashed"
+                  borderColor="blackAlpha.300"
+                  borderRadius={2}
+                  bg="gray.50"
+                  as={VStack}
+                >
+                  <Text fontSize="xs" color="gray.600" fontWeight="semibold">
+                    Drop files here or
+                  </Text>
+                  <Button
+                    leftIcon={<Icon as={PiUploadBold} />}
+                    size="xs"
+                    bg="white"
+                    border="1px solid"
+                    borderColor="gray.300"
+                    px={3}
+                    py={4}
+                    mt={1}
+                    _hover={{
+                      bg: "blackAlpha.100",
+                      borderColor: "gray.400",
+                    }}
+                    onClick={onButtonClick}
+                  >
+                    Select File
+                  </Button>
+                  <input
+                    type="file"
+                    id="file"
+                    ref={inputFile}
+                    style={{ display: "none" }}
+                    onChange={(event) =>
+                      fileSelectHandler(
+                        event.target.files ? event.target.files[0] : null
+                      )
+                    }
+                  />
+                </Box>
+              </Box>
+              <Box
+                w="full"
+                h={20}
+                mt={2}
+                bg="#eaddd7"
+                border="2px solid black"
+              />
+            </GridItem>
+            <GridItem w="full" colSpan={3}>
+              <Box
+                w="full"
+                h="full"
+                mt={0}
+                bg="white"
+                border="2px solid black"
+                as={Flex}
+                justifyContent="center"
+                alignItems="center"
+              >
+                {selectedImage && (
+                  <div>
+                    <img
+                      alt="not found"
+                      width={"500px"}
+                      src={URL.createObjectURL(selectedImage)}
+                    />
+                    <br />
+                  </div>
+                )}
+              </Box>
+            </GridItem>
+            <GridItem w="full" colSpan={1}>
+              <Box
+                w="full"
+                h={32}
+                mt={0}
+                bg="#eebefa"
+                border="2px solid black"
+              />
+              <Box
+                w="full"
+                h={60}
+                mt={2}
+                bg="white"
+                border="2px solid black"
+                px={2}
+                py={2}
+              >
+                <Text fontSize="sm" fontWeight="semibold">
+                  Bottle list :
+                </Text>
+                <OrderedList pl={4}>
+                  {data &&
+                    data.predictions.map((currentValue, index, arr) => {
+                      return (
+                        <>
+                          <ListItem
+                            key={index}
+                            style={{
+                              fontSize: "12px",
+                            }}
+                          >
+                            {currentValue.class}
+                          </ListItem>
+                        </>
+                      );
+                    })}
+                </OrderedList>
+                <Text fontSize="sm" fontWeight="semibold" mt={4}>
+                  Price : {price}
+                </Text>
+              </Box>
+            </GridItem>
+          </Grid>
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default Home;
